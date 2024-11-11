9a4e00578e28f35443a262236d75b4ca8ff2fc08
vorarsh <vorarsh@gmail.com>
2024-11-11 17:56:47 +0100

    Changes
    Analyzing economic indicators for BRICS nations using Python, data visualization, and predictive modeling.

---
+++ venv/Scripts/data_import.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import os
+
+data_path = 'C:/Users/ARSH/brics-economic-analysis/venv/data/raw'
+
+files = os.listdir(data_path)
+print(f"Total files: {len(files)}")
+
+sample_file = os.path.join(data_path, files[0])
+df = pd.read_csv(sample_file)
+
+print("Sample Data:")
+print(df.head())
+print("\nColumns:",df.columns)
